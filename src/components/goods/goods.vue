5fb4a347b67ec690e5cbd93944b4133ef7a324fe
Lix <xiao.liunit@gmail.com>
2017-04-12 17:09:53 +0800

    header

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="goods">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'goods',
+    data () {
+      return {
+        msg: '这是一个商品列表!!!'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Goods from '@/components/goods/goods.vue'
+import Ratings from '@/components/ratings/ratings.vue'
+import Seller from '@/components/seller/seller'
+
+Vue.use(Router)
+
+const routes = [
+  { path: '/goods', component: Goods },
+  { path: '/ratings', component: Ratings },
+  { path: '/seller', component: Seller }
+]
+
+export default new Router({
+  routes,
+  linkActiveClass: 'active'
+})
