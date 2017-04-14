1cdb13831019d2c2e2ea6fd028feb4d290a6fa97
Lix <xiao.liunit@gmail.com>
2017-04-14 14:05:51 +0800

    购物车状态变更

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,17 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{ 'highlight' : totalCount > 0 }">
+            <i class="icon-shopping_cart" :class="{ 'highlight' : totalCount > 0 }"></i>
           </div>
+          <div class="num" v-show="totalCount > 0">{{ totalCount }}</div>
         </div>
-        <div class="price">￥{{ totalPrice }}元</div>
+        <div class="price" :class="{ 'highlight' : totalPrice > 0 }">￥{{ totalPrice }}元</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{ minPrice }}元起送
+        <div class="pay" :class="payClass">
+          {{ payDesc }}
         </div>
       </div>
     </div>
@@ -25,7 +26,12 @@
       selectFoods: {
         type: Array,
         default () {
-          return []
+          return [
+            {
+              price: 22,
+              count: 1
+            }
+          ]
         }
       },
       deliveryPrice: {
@@ -44,6 +50,30 @@
           total += food.price * food.count
         })
         return total
+      },
+      totalCount () {
+        let count = 0
+        this.selectFoods.forEach((food) => {
+          count += food.count
+        })
+        return count
+      },
+      payDesc () {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice
+          return `还差￥${diff}元起送`
+        } else {
+          return '去结算'
+        }
+      },
+      payClass () {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough'
+        } else {
+          return 'enough'
+        }
       }
     }
   }
@@ -76,6 +106,20 @@
           box-sizing: border-box
           border-radius: 50%
           background: #141d27
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            color: #fff
+            background: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
           .logo
             width: 100%
             height: 100%
@@ -83,10 +127,14 @@
             box-sizing: border-box
             text-align: center
             background: #2b343c
+            &.highlight
+              background: rgb(0, 160, 220)
             .icon-shopping_cart
               line-height: 44px
               font-size: 24px
               color: #80858a
+              &.highlight
+                color: #fff
         .price
           display: inline-block
           vertical-align: top
@@ -97,9 +145,11 @@
           border-right: 1px solid rgba(255, 255, 255, 0.1)
           font-size: 16px
           font-weight: 700
+          &.highlight
+            color: #fff
         .desc
           display: inline-block
-          vertical-align:top
+          vertical-align: top
           line-height: 24px
           margin: 12px 0 0 12px
           font-size: 10px
@@ -108,9 +158,14 @@
         width: 105px
         .pay
           height: 48px
-          line-height:48px
+          line-height: 48px
           text-align: center
           font-size: 12px
-          font-weight:700
+          font-weight: 700
           background: #2b333b
+          &.not-enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: #fff
 </style>
