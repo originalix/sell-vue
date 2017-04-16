2973757ade2913dafb1e9788c979049faacc6681
Lix <xiao.liunit@gmail.com>
2017-04-17 07:52:44 +0800

    购物车小球动画

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,7 +28,7 @@
                   <span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
                 </div>
                 <div class="cartcontrol-wrapper">
-                  <cartcontrol :food="food"></cartcontrol>
+                  <cartcontrol @add="addFood" :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -36,7 +36,7 @@
         </li>
       </ul>
     </div>
-    <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
@@ -104,6 +104,15 @@
         let foodList = this.$refs.foodList
         let el = foodList[index]
         this.foodsScroll.scrollToElement(el, 300)
+      },
+      addFood (target) {
+        this._drop(target)
+      },
+      _drop (target) {
+        // 体验优化，异步执行下落动画
+        this.$nextTick(() => {
+          this.$refs.shopcart.drop(target)
+        })
       },
       _initScroll () {
         this.menuScroll = new BScroll(this.$refs.menuWrapper, {
