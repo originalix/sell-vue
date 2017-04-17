05b2ca96ddd127fd7c8058bf03870a52e7bdc576
Lix <xiao.liunit@gmail.com>
2017-04-17 16:49:53 +0800

    商品详情页

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -2,8 +2,22 @@
   <transition name="move">
     <div class="food" v-show="showFlag" ref="food">
       <div class="food-content">
-        <div class="iamge-header">
-
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{ food.name }}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{ food.sellCount }}份</span>
+            <span class="rating">好评率{{ food.rating }}%</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice
+            }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +39,9 @@
     methods: {
       show () {
         this.showFlag = true
+      },
+      hide () {
+        this.showFlag = false
       }
     }
   }
@@ -44,4 +61,54 @@
       transition: all 0.2s linear
     &.move-enter, &.move-leave-active
       transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #fff
+    .content
+      position: relative
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
 </style>
