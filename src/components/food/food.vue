b4f83632bad0b8fe1230827b810428386e5d75e3
Lix <xiao.liunit@gmail.com>
2017-04-17 17:24:26 +0800

    详情页加入购物车

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,6 +18,12 @@
             <span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice
             }}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+          </transition>
         </div>
       </div>
     </div>
@@ -25,6 +31,10 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import Vue from 'vue'
+  import cartcontrol from 'components/cartcontrol/cartcontrol'
+
   export default {
     props: {
       food: {
@@ -39,10 +49,29 @@
     methods: {
       show () {
         this.showFlag = true
+        this.$nextTick(() => {
+          if (!this.scrool) {
+            this.scrool = new BScroll(this.$refs.food, {
+              click: true
+            })
+          } else {
+            this.scrool.refresh()
+          }
+        })
       },
       hide () {
         this.showFlag = false
+      },
+      addFirst (event) {
+        if (!event._constructed) {
+          return
+        }
+        this.$emit('add', event.target)
+        Vue.set(this.food, 'count', 1)
       }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
@@ -111,4 +140,28 @@
           text-decoration: line-through
           font-size: 10px
           color: rgb(147, 153, 159)
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        opacity: 1
+      &.fade-enter-active, &.fade-leave-active
+        transition: all 0.2s
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+        z-index: -1
+
 </style>
