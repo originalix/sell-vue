8a2bb9ddbe2e87f4c4d3dfb3cbf78bad8ae4decf
Lix <xiao.liunit@gmail.com>
2017-04-18 13:27:01 +0800

    ratingselect ratings

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span @click="select(2, $event)" class="block positive" :class="{'active':selectType===2}">{{ desc.all }}<span class="count">57</span></span>
-      <span @click="select(0, $event)" class="block positive" :class="{'active':selectType===0}">{{ desc.positive }}<span class="count">47</span></span>
-      <span @click="select(1, $event)" class="block negative" :class="{'active':selectType===1}">{{ desc.negative }}<span class="count">10</span></span>
+      <span @click="select(2, $event)" class="block positive" :class="{'active':selectType===2}">{{ desc.all }}<span class="count">{{ ratings.length }}</span></span>
+      <span @click="select(0, $event)" class="block positive" :class="{'active':selectType===0}">{{ desc.positive }}<span class="count">{{ positives.length }}</span></span>
+      <span @click="select(1, $event)" class="block negative" :class="{'active':selectType===1}">{{ desc.negative }}<span class="count">{{ negatives.length }}</span></span>
     </div>
     <div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
       <span class="icon-check_circle"></span>
@@ -13,8 +13,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-  // const POSITIVE = 0
-  // const NEGATIVE = 1
+  const POSITIVE = 0
+  const NEGATIVE = 1
   const ALL = 2
 
   export default {
@@ -42,6 +42,18 @@
             negative: '不满意'
           }
         }
+      }
+    },
+    computed: {
+      positives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE
+        })
+      },
+      negatives () {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE
+        })
       }
     },
     methods: {
