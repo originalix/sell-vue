a1a0454120900a34c030e92cf465bfe930610807
Lix <xiao.liunit@gmail.com>
2017-04-18 11:03:18 +0800

    ratingselect component

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -31,6 +31,10 @@
           <p class="text">{{ food.info }}</p>
         </div>
         <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -41,6 +45,7 @@
   import Vue from 'vue'
   import cartcontrol from 'components/cartcontrol/cartcontrol'
   import split from 'components/split/split'
+  import ratingselect from 'components/ratingselect/ratingselect'
 
   export default {
     props: {
@@ -82,7 +87,8 @@
     },
     components: {
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
   }
 </script>
---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="ratingselect">
+    <div class="rating-type">
+      <span>{{ desc.all }}</span>
+      <span>{{ desc.positive }}</span>
+      <span>{{ desc.negative }}</span>
+    </div>
+    <div class="switch">
+      <span class="icon-check_circle"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // const POSITIVE = 0
+  // const NEGATIVE = 1
+  const ALL = 2
+
+  export default {
+    props: {
+      ratings: {
+        type: Array,
+        default () {
+          return []
+        }
+      },
+      selectType: {
+        type: Number,
+        default: ALL
+      },
+      onlyContent: {
+        type: Boolean,
+        default: false
+      },
+      desc: {
+        type: Object,
+        default () {
+          return {
+            all: '全部',
+            positive: '满意',
+            negative: '不满意'
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .ratingselect
+    .rating-type
+      padding: 18px 0
+      margin: 0 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      font-size: 18px
+  .switch
+    padding: 12px 18px
+    line-height: 24px
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    color: rgb(147, 153, 159)
+    font-size:0
+    .icon-check_circle
+      display: inline-block
+      vertical-align: top
+      margin-right: 4px
+      font-size: 24px
+    .text
+      display: inline-block
+      vertical-align: top
+      font-size: 12px
+</style>
