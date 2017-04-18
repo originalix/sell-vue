9456dfa61e63564ec12182c7b9ea48b253b4d708
Lix <xiao.liunit@gmail.com>
2017-04-18 11:47:53 +0800

    selectRating type css and bind data

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span>{{ desc.all }}</span>
-      <span>{{ desc.positive }}</span>
-      <span>{{ desc.negative }}</span>
+    <div class="rating-type border-1px">
+      <span class="block positive" :class="{'active':selectType===2}">{{ desc.all }}<span class="count">57</span></span>
+      <span class="block positive" :class="{'active':selectType===0}">{{ desc.positive }}<span class="count">47</span></span>
+      <span class="block negative" :class="{'active':selectType===1}">{{ desc.negative }}<span class="count">10</span></span>
     </div>
     <div class="switch">
       <span class="icon-check_circle"></span>
@@ -56,12 +56,33 @@
       margin: 0 18px
       border-1px(rgba(7, 17, 27, 0.1))
       font-size: 18px
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        line-height: 16px
+        border-radius: 1px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
   .switch
     padding: 12px 18px
     line-height: 24px
     border-bottom: 1px solid rgba(7, 17, 27, 0.1)
     color: rgb(147, 153, 159)
-    font-size:0
+    font-size: 0
     .icon-check_circle
       display: inline-block
       vertical-align: top
