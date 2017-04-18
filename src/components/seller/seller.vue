8f78f6c9a996e968c50c16e977aea9799ff7e876
Lix <xiao.liunit@gmail.com>
2017-04-19 07:05:40 +0800

    seller layout

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="seller">
-    I am a seller
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{ seller.name }}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">{{ seller.ratingCount }}</span>
+          <span class="text">月售{{ seller.sellCount }}单</span>
+        </div>
+        <ul class="remark">
+          
+        </ul>
+      </div>
+      <split></split>
+      <div class="bulletin">公告</div>
+      <split></split>
+      <div class="pics">图片</div>
+      <split></split>
+      <div class="info">商家信息</div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import split from 'components/split/split'
+  import star from 'components/star/star'
+
+  export default {
+    components: {
+      star,
+      split
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
