53782c751de39bbb16dcf7c30e3e6fa10527044d
Lix <xiao.liunit@gmail.com>
2017-04-18 17:16:41 +0800

    ratings comment

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -26,13 +26,30 @@
       </div>
       <split></split>
       <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul v-show="ratings && ratings.length">
+          <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in ratings" class="rating-item">
+            <div class="user">
+              <span class="name">{{ rating.username }}</span>
+              <img class="avatar" width="12" height="12" :src="rating.avatar">
+            </div>
+            <div class="time">{{ rating.rateTime | formatDate }}</div>
+            <p class="text">
+                  <span
+                    :class="{'icon-thumb_up':rating.rateType===0, 'icon-thumb_down':rating.rateType===1}"></span>{{ rating.text
+              }}
+            </p>
+          </li>
+        </ul>
+        <div class="no-rating" v-show="!ratings || !ratings.length">暂无评价</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
-  //  import {formatDate} from 'common/js/date'
+  import {formatDate} from 'common/js/date'
   import star from 'components/star/star'
   import ratingselect from 'components/ratingselect/ratingselect'
   import split from 'components/split/split'
@@ -90,6 +107,12 @@
         })
       }
     },
+    filters: {
+      formatDate (time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
+      }
+    },
     components: {
       star,
       ratingselect,
@@ -99,6 +122,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .ratings
     position: absolute
     top: 174px
@@ -161,4 +186,45 @@
             margin-left: 12px
             font-size: 12px
             color: rgb(147, 153, 159)
+    .rating-wrapper
+      padding: 0 18px
+    .rating-item
+      position: relative
+      padding: 16px 0
+      border-1px(rgba(7, 17, 27, 0.1))
+      .user
+        position: absolute
+        right: 0
+        top: 16px
+        line-height: 12px
+        font-size: 0
+        .name
+          display: inline-block
+          margin-right: 6px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .avatar
+          border-radius: 50%
+      .time
+        margin-bottom: 6px
+        line-height: 12px
+        font-size: 10px
+        color: rgb(147, 153, 159)
+      .text
+        line-height: 16px
+        font-size: 12px
+        color: rgb(7, 17, 27)
+        .icon-thumb_up, .icon-thumb_down
+          margin-right: 4px
+          line-height: 16px
+          font-size: 12px
+        .icon-thumb_up
+          color: rgb(0, 160, 220)
+        .icon-thumb_down
+          color: rgb(147, 153, 159)
+    .no-rating
+      padding: 16px 0
+      font-size: 12px
+      color: rgb(147, 153, 159)
 </style>
