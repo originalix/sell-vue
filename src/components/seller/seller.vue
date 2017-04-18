617a030a4328ace481d0287f31afbe63624919ba
Lix <xiao.liunit@gmail.com>
2017-04-19 07:18:25 +0800

    seller header css

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -9,7 +9,24 @@
           <span class="text">月售{{ seller.sellCount }}单</span>
         </div>
         <ul class="remark">
-          
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{ seller.minPrice }}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
         </ul>
       </div>
       <split></split>
@@ -27,6 +44,11 @@
   import star from 'components/star/star'
 
   export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
     components: {
       star,
       split
@@ -35,5 +57,56 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
 
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      position: relative
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-right: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          margin-right: 12px
+          line-height: 18px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(77, 85, 93)
+      .remark
+        display: flex
+        padding-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7, 17, 27, 0.1)
+          &last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+            .stress
+              font-size: 24px
 </style>
